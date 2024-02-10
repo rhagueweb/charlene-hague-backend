33e3de66c5f97d552c2020a1bcee24d588099188
Richard Hague <rhagueweb@gmail.com>
2024-02-10 15:52:22 +0000

    First commit

---
+++ src/collections/Pages.js
@@ -0,0 +1,30 @@
+export const Pages = {
+  slug: 'pages',
+  labels: {
+    singular: 'Page',
+    plural: 'Pages',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      label: 'Slug',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'layout',
+      label: 'Layout',
+      type: 'blocks',
+      blocks: [],
+    },
+  ],
+};
